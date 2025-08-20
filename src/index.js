16874eea435b30c6692b7bc7049c7b8fb7f278bc
DashaDashaD <111508812+DashaDashaD@users.noreply.github.com>
2025-08-20 21:24:36 +0300

    Changed prompt and context

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "0bb94a006ac75c20203ba5cf3b3o40dt";
-  let prompt = `Generate a short English poem about ${instructionsInput.value}`;
-  let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 6 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `Generate a short poem about ${instructionsInput.value}. Try to recognize language of provided topic`;
+  let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 6 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a romantic poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem).catch((error) => {
     poemElement.innerHTML = "❌ Something went wrong. Please try again.";
